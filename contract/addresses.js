a44707318813759061f17348854c5fd5f3fbec8f
Syed Mustajab Abbas Nqvi <wasiabbas981@gmail.com>
2022-06-17 21:03:01 +0500

    [UPDATE] NFT FBI and addresses

--- contract/addresses.js
+++ contract/addresses.js
@@ -1,4 +1,4 @@
 
- export const islandGirl_addr = "0x1661000dBe43d2B06544B0857685D99b825b975B"
- export const igirlNFT_addr = "0xb0D2Cf0ae4094A496919314b34ab3546556C05b8"
+ export const islandGirl_addr = "0xb5763b4ab4206C96D80EC630cC179A5252508d1F"
+ export const igirlNFT_addr = "0x58A83B18c4120d4D38Ca232ABB6153BF3b8D1B37"
  export const islandGirlGame_addr = "0x595B4418303CA52823BfcBcB3ff3E3C63A75585e"
