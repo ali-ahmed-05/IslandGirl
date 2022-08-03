7ff466b410cd4f36ea99bdcab2dbffb9e893a47f
JunaidSikander <junaid_sikander@live.com>
2022-08-03 17:45:47 +0500

    fix :bug:

--- public/assets/js/Scenes/CharacterSelection.js
+++ public/assets/js/Scenes/CharacterSelection.js
@@ -1,25 +1,26 @@
-import { map } from "jquery";
-
 export default class characterSelection extends Phaser.Scene {
     constructor() {
-        super('characterSelection');
+        super("characterSelection");
     }
 
     init(data) {
         this.players = [];
         if (data?.userNFTsNames.length === 1) {
-
             const player = data.userNFTsNames[0].name;
-            return this.scene.start('level_1', { player });
+            return this.scene.start("level_1", {player});
         }
-        let unqiuePlayers = data?.userNFTsNames.map(metadeta => metadeta.name)
-        unqiuePlayers = [... new Set(unqiuePlayers)]
 
-        this.players = data.userNFTsNames;
+        this.players = (() => {
+            const playerNames = data?.userNFTsNames.map(obj => obj.name);
+            return [...new Set(playerNames)];
+        })()
     }
 
     preload() {
-        this.load.image('character_bg', 'assets/images/characters/character_bg.png');
+        this.load.image(
+            "character_bg",
+            "assets/images/characters/character_bg.png"
+        );
         this.load.image("Alice", "../assets/images/characters/Alice.png");
         this.load.image("Chralotte", "../assets/images/characters/Chralotte.png");
         this.load.image("Esmeralda", "../assets/images/characters/Esmeralda.png");
@@ -36,52 +37,69 @@ export default class characterSelection extends Phaser.Scene {
         this.background.displayWidth = innerWidth;
         this.background.displayHeight = innerHeight;
 
-        const characters = []
+        const characters = [];
         const charactersTitle = [];
         let gape = 0;
 
         this.players.forEach(function (player, index) {
             gape += 200;
 
-            characters[index] = ref.add.image(gape, innerHeight / 2, player.name);
-            ref.add.text(index === 0 ? (gape - 40) : gape - 20, (innerHeight / 2) + 100, player.name, {
-                fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif',
-            });
+            characters[index] = ref.add.image(gape, innerHeight / 2, player);
+            ref.add.text(
+                index === 0 ? gape - 40 : gape - 20,
+                innerHeight / 2 + 100,
+                player,
+                {
+                    fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif',
+                }
+            );
 
             characters[index].setScale(0.2);
 
-            characters[index].on('pointerover', () => {
-                characters[index].setScale(0.22)
-            })
-            characters[index].on('pointerout', () => {
-                characters[index].setScale(0.2)
-            })
+            characters[index].on("pointerover", () => {
+                characters[index].setScale(0.22);
+            });
+            characters[index].on("pointerout", () => {
+                characters[index].setScale(0.2);
+            });
         });
 
-        ref.add.text((innerWidth / 2) * 0.75, (innerHeight / 2) * 0.5, 'Please Choose Your Player', {
-            fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif',
-            fontSize: '3em'
-            // color: '#F7B32A',
-        });
+        ref.add.text(
+            (innerWidth / 2) * 0.75,
+            (innerHeight / 2) * 0.5,
+            "Please Choose Your Player",
+            {
+                fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif',
+                fontSize: "3em",
+                // color: '#F7B32A',
+            }
+        );
 
         characters.forEach(function (character) {
-            ref.onCharacterSelect(character, character.texture.key)
+            ref.onCharacterSelect(character, character.texture.key);
         });
-
     }
 
-
     onCharacterSelect(btn, player) {
-        btn.setInteractive()
-        btn.on('pointerdown', () => {
+        btn.setInteractive();
+        btn.on("pointerdown", () => {
+            this.time.delayedCall(
+                100,
+                function () {
+                    this.cameras.main.fadeOut(1000, 0, 0, 0);
+                },
+                [],
+                this
+            );
 
-            this.time.delayedCall(100, function () {
-                this.cameras.main.fadeOut(1000, 0, 0, 0)
-            }, [], this);
-
-            this.time.delayedCall(2000, function () {
-                this.scene.start('level_1', { player });
-            }, [], this);
-        })
+            this.time.delayedCall(
+                2000,
+                function () {
+                    this.scene.start("level_1", {player});
+                },
+                [],
+                this
+            );
+        });
     }
 }
